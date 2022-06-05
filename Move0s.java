3c5173fb1b3afb3caaa8b07a6d13e9913259777e
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-05 17:00:43 +0530

    Create Move0s.java

---
+++ Move0s.java
@@ -0,0 +1,16 @@
+Class main{
+ if(array.length == 0|| array.length ==1)
+   return;
+ int left = 0;
+  int right = 0;
+  for(int i = 0 ; i<array.length;i++)
+    if(array[right[ == 0)
+            right++;
+    else{
+      int temp = array[left]
+      array[left] = array[right]
+       array[right] = temp;
+      right++;
+      left++;
+  
+}
