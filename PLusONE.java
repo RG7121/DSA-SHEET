4e4ac8b6314b9529860fef846386a7f3c49a78dc
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-05 17:45:51 +0530

    Create PLusONE.java

---
+++ PLusONE.java
@@ -0,0 +1,15 @@
+class Main{
+  for(int i = arr.length-1;i>=0;i++)
+   if(arr[i] != 9)
+     arr[i]++;
+     break;
+  else 
+    arr[i] =0;
+  
+  if(arr[0]==0)
+    int[] result = new int[arr.length+1]
+   result[0]  =1;
+  return result
+    
+  return arr;
+  }
