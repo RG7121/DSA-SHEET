bf15d41db34f51e96b65f810b46c9907b0cf587d
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-05 19:38:20 +0530

    Create REMOVEduplicatesSORTED.java

---
+++ REMOVEduplicatesSORTED.java
@@ -0,0 +1,11 @@
+class MAin{
+  if(nums.length ==0)
+    return 0;
+}
+int i = 0;
+for(int j = 0; j<array.length;j++)
+  if(nums[i} != nums[j})
+         i++;
+      nums[i} =nums[j}
+                    
+  return i+1;
