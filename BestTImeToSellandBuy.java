ab3b8e3de310d0354cc3fd171f8e89bf7ae3ad5e
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-05 15:37:28 +0530

    Create BestTImeToSellandBuy.java

---
+++ BestTImeToSellandBuy.java
@@ -0,0 +1,14 @@
+class main{
+  int least = Integer.MAX_VALUE;
+  profit = 0;
+  profitToday = 0;
+  for(int i = 0 ; array.lenght; i++)
+   if(array[i] <least) 
+     least = array[i];
+ priceToday = array[i] - least;
+  if(profit<priceTOday)
+    profit = priceTOday;
+}
+return profit;
+
+time and space -- o(n);
