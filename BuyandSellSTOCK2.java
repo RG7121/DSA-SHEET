aeb522a68859b509888eeac8f6c1ba8d0e520411
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-06 17:02:30 +0530

    Create BuyandSellSTOCK2.java

---
+++ BuyandSellSTOCK2.java
@@ -0,0 +1,10 @@
+Class Main{
+if(arr.length == 0) return 0;
+  int profit = 0;
+ for(int i = 0 ; i<array.length -1;i++)
+   if(array[i] <array[i+1])
+     profit+= array[i+1] - array[i];
+
+}
+return profit;
+
