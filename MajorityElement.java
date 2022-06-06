551c7d67ca60adc667a889863623d1df98d495c8
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-06 16:07:47 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,13 @@
+class MAin{ // BOYER MOORE ALGORITHM
+int major = nums[0]
+ count = 1;
+  for(int i = 1; i<nums.length ;i++)
+    if(major == nums[i+1])
+      count ++:
+  else{
+    count--;
+    
+  if(count == 0_
+     major = nums[i+1];
+  return major;
+}
