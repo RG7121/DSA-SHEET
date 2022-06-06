57b873b213601b2b01f1ff709cb11bc050340a20
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-06 14:18:29 +0530

    Create SquareOfSORtedarr.java

---
+++ SquareOfSORtedarr.java
@@ -0,0 +1,17 @@
+class Main{
+ int i = 0;
+  int j = arr.length-1;
+  int k = arr.length-1;
+  int[] result = new int[n];
+  while(i<=j){
+    if(MAth.abs(nums[i]) > Math.abs(nums[j])
+       result[k] = nums[i]*nums[i];
+       i++;
+     else
+       result[k] = nums[j]*nums[j];
+       j--;
+   k--;
+       
+ return result;
+
+}
