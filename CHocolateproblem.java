cea75b5ad063e61e9741a7db1d654a34d9024a86
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-06 17:44:11 +0530

    Create CHocolateproblem.java

---
+++ CHocolateproblem.java
@@ -0,0 +1,13 @@
+class Main{
+ if( students ==0 | packets == 0)
+   return 0;
+  if( students> packets) 
+    return -1;
+  int min = Integer.MAX_VALUE;
+  for(int i = 0; i+m-1 <n ;I++)
+    int diff = arr[i+m-1] -arra[i]
+    if(diff <min)
+      min = diff;
+  
+  return diff;
+}
