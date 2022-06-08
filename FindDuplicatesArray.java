09fd23ebe55daa3185cee63b3c1bd286bd55e0ee
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-08 17:31:50 +0530

    Create FindDuplicatesArray.java

---
+++ FindDuplicatesArray.java
@@ -0,0 +1,9 @@
+Class Main{
+     list<Integer> rs = new Arraylist<>();
+   for(int i = 0; i<array.length;i++)
+     int index = Math.abs(nums[i]) -1;
+    if(nums[index] <0)
+          rs.add(nums[i]);
+     nums[index] = -nums[inde];
+  return rs;
+}
