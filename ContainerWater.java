56aa786f4f6c960254e571c342c9b7563b25384c
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-08 21:24:34 +0530

    Create ContainerWater.java

---
+++ ContainerWater.java
@@ -0,0 +1,11 @@
+Class Main{
+ int max = 0;
+  int i = 0;
+  int j = array.length-1;
+  while(i<j)
+    max = Math.max(max, Math.min(arr[i],arr[j]* (j-i));
+    if(i<j) i++;
+    else j--;
+                   
+                  return max
+}
