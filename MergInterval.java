8cdc89f374d71d119aa3534ae4300c651fb86b2c
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-08 05:59:15 +0530

    Create MergInterval.java

---
+++ MergInterval.java
@@ -0,0 +1,15 @@
+class Main{
+  list<int[]> rs = new Arraylist<> ();
+  if(array.length == 0 || array == null){  return rs.toArray(new int[0][1]);
+  Array.sort(array, (a,b) -> a[0]-b[0]);
+        int start = intervals[0][0]; int end = intervals[0][1];
+     for(int i ;intervals){
+        if(i[i] <= end){
+           end = Math.max(intervals, i[i])}
+       else{
+         
+       //or array
+  
+  
+}
+//lodu questions of google just remember this part and approach 
