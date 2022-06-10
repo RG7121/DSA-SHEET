f8d87e8a9b07131a7177f6711d2a4076c2fa1059
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-10 20:38:20 +0530

    Create NextPermutation.java

---
+++ NextPermutation.java
@@ -0,0 +1,15 @@
+class Main{
+  if(nums.length == 0|| nums == null) return;
+  int i = nums.length-2;
+  while(i>=0 && nums[i] >= nums[i+1]) i--;
+  if(i>=0){
+    int j = arr.length-1;
+    while(a[j] <=a[i])
+    j--;
+    swap(nums,i,j);
+}
+reverse(nums,i+1,arr.length-1);
+
+
+public static void swap
+public static void reverse while(i<j) reverse
