489faac7649360afbeb273feaac97ec5fcb1a899
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-13 06:16:27 +0530

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,8 @@
+Class Main{
+ int reach = 0;
+  for(int i = 0 ; i<= reach;i++)
+    reach = Maht.max(reach, numspi[ + i);
+      if( reach >= nums.length-1);
+                   return true;
+    return falsel;
+}
