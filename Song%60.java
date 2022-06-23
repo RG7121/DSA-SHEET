e347a47886dc587b11c9c552575dc043bc5b2605
Rishabh Gulati <gulati.r7121@gmail.com>
2022-06-23 18:06:47 +0530

    Create Song%60.java

---
+++ Song%60.java
@@ -0,0 +1,11 @@
+Class Main{
+  Map<Integer,Integer> map = new HashMap<>();
+ int ans = 0;
+  for(int t ; array) 
+    int b = t%60;
+   int a = ( b == 0) ?0 : 60-b;
+  ans += map.getORDefault( a,0)
+    map.put(t%60: map.getORDefault(t%60,0));
+  
+return ans;
+}
